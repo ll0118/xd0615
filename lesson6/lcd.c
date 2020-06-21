b64a4477559ebf01e3d5cb5ec1a143d28ddfdc82
ll <1606045209@qq.com>
2020-06-21 13:38:35 +0800

    第6次作业提交 吕陆 19030100179

---
+++ lesson6/lcd.c
@@ -0,0 +1,26 @@
+#include<LiquidCrystal.h>
+
+LiquidCrystal lcd(12,11,5,4,3,2);
+
+void setup()
+{
+  lcd.begin(16,2);
+  
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  if(Serial.available())
+  {
+    delay(100);
+    lcd.clear();
+    lcd.setCursor(1,1);
+   while(Serial.available()>0)
+    {
+      lcd.write(Serial.read());
+      lcd.print(Serial.readString());
+    }
+    
+  }
+}
